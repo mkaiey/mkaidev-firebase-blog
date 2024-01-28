e6ce3fa0c3e61c1b869ed7a36e455cce32664de7
novipro88 <noviantipali97@gmail.com>
2024-01-29 06:34:03 +0700

    fixed code

--- src/components/Demo/Discover.jsx
+++ src/components/Demo/Discover.jsx
@@ -19,9 +19,9 @@ const Discover = () => {
             </button>
           ))}
         </div>
-        <button className="text-green-600 text-sm py-3 hover:text-black1">
+        {/* <button className="text-green-600 text-sm py-3 hover:text-black1">
           See more topics
-        </button>
+        </button> */}
       </div>
       <div className="flex items-center flex-wrap gap-3 leading-3 pt-8">
         {discoverActions.map((item, i) => (
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Posts from "../Common/Posts/Posts";
 import Follow from "./UserToFollow/Follow";
+import Discover from "../Demo/Discover";
 
 const Home = () => {
   return (
@@ -7,9 +8,10 @@ const Home = () => {
       <div className="flex-[2] py-10 mb-[4rem]">
         <Posts />
       </div>
-      <div className="hidden md:inline-block md:w-[21rem] p-7 border-l border-gray-300">
+      <div className="hidden md:inline-block md:w-[21rem] p-8 border-l border-gray-300">
         <h3>Who to follow?</h3>
         <Follow />
+        <Discover />
       </div>
     </section>
   );
